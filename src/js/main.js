ba651c3e8d07331d54529bc4a3e7ad1c13249a2e
Alyx11 <aleks78.gooogle@gmail.com>
2020-04-29 12:34:36 +0300

    added products

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
 $(function(){
 
+  $('input[type="file"], select').styler();
+
+  var mixer = mixitup('.products__inner',{
+   load: {
+     filter: '.category-drinks, .category-bake, .category-dishes'
+   }
+  });
+
 });
